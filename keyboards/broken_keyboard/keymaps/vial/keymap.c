1e0ca5a4cefd00833224a5c15c19e487139afbbf
sagepeppermint <101568628+sagepeppermint@users.noreply.github.com>
2022-11-13 20:40:13 +1100

    Update keymap.c

--- keyboards/broken_keyboard/keymaps/vial/keymap.c
+++ keyboards/broken_keyboard/keymaps/vial/keymap.c
@@ -3,20 +3,20 @@
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     [0] = LAYOUT_5x7(
-        KC_ESC, KC_1, KC_2, KC_3, KC_4, KC_5, XXXXXXX,
-                XXXXXXX, KC_6, KC_7, KC_8, KC_9, KC_0, RESET,
+        KC_ESC, KC_1, KC_2, KC_3, KC_4, KC_5, KC_MINUS,
+                KC_EQUAL, KC_6, KC_7, KC_8, KC_9, KC_0, KC_DEL,
         
-        KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, XXXXXXX,
-                XXXXXXX, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_BSPC,
+        KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_LEFT_BRACKET,
+                KC_RIGHT_BRACKET, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_BACKSPACE,
         
-        KC_LCTL, KC_A, KC_S, KC_D, KC_F, KC_G, XXXXXXX,
-                XXXXXXX, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT,
+        OSM(MOD_LCTL), KC_A, KC_S, KC_D, KC_F, KC_G, KC_PAGE_UP,
+                KC_BACKSLASH, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT,
         
-        KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B, XXXXXXX,
-                XXXXXXX, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_RSFT,
+        OSM(MOD_LSFT), KC_Z, KC_X, KC_C, KC_V, KC_B, KC_PAGE_DOWN,
+                KC_GRAVE, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, OSM(MOD_RSFT),
         
-        KC_LCTL, KC_LGUI, KC_LALT, XXXXXXX, XXXXXXX, KC_ENT, MO(1),
-                XXXXXXX, MO(2), XXXXXXX, KC_SPC, KC_RALT, KC_RGUI, KC_RCTL
+        KC_LCTL, KC_LGUI, KC_LALT, KC_1, KC_2, KC_ENT, KC_3,
+                KC_4, KC_SPACE, KC_5, KC_6, KC_RALT, KC_RGUI, KC_RCTL
         
     ),
 
