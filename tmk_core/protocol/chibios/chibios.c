3d85565999d6b4e915b034f68f093db2ac6dd728
sagepeppermint <101568628+sagepeppermint@users.noreply.github.com>
2023-04-03 23:05:32 +1000

    Update chibios.c
    to fix broken wakeup

--- tmk_core/protocol/chibios/chibios.c
+++ tmk_core/protocol/chibios/chibios.c
@@ -17,6 +17,7 @@
 
 #include <ch.h>
 #include <hal.h>
+#include <core_cm4.h>
 
 #include "usb_main.h"
 
@@ -198,6 +199,7 @@ void protocol_pre_task(void) {
         /* Woken up */
         // variables has been already cleared by the wakeup hook
         send_keyboard_report();
+        NVIC_SystemReset();
 #    ifdef MOUSEKEY_ENABLE
         mousekey_send();
 #    endif /* MOUSEKEY_ENABLE */
