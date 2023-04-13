acc3370ee77f20b3b421d876d4dbbc866aa48c63
sagepeppermint <101568628+sagepeppermint@users.noreply.github.com>
2023-04-13 18:37:06 +1000

    Update config.h

--- keyboards/shahnetwork/b/config.h
+++ keyboards/shahnetwork/b/config.h
@@ -61,9 +61,10 @@
 #endif
 
 /* fix USB timeout issues */
-#define SPLIT_USB_TIMEOUT 5000 // 2000 is default
-#define SPLIT_USB_TIMEOUT_POLL 100 // 10 is default
-#define USB_SUSPEND_WAKEUP_DELAY 200 // 0 is default
+//#define SPLIT_USB_TIMEOUT 5000 // 2000 is default
+//#define SPLIT_USB_TIMEOUT_POLL 100 // 10 is default
+//#define USB_SUSPEND_WAKEUP_DELAY 200 // 0 is default
+#define SPLIT_WATCHDOG_ENABLE
 
 /* typing options */
 /* caps word */
@@ -78,5 +79,3 @@
 /* tapping settings */
 #define TAPPING_TERM 200
 
-/* fix wakeup issues */
-#define USB_SUSPEND_WAKEUP_DELAY 200
