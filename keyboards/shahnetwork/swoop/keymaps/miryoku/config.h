1627d6109e96cce9845b333b55596f24578451d6
sagepeppermint <101568628+sagepeppermint@users.noreply.github.com>
2023-04-16 17:10:54 +1000

    OK

---
+++ keyboards/shahnetwork/swoop/keymaps/miryoku/config.h
@@ -0,0 +1,11 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0xFD, 0x91, 0xD2, 0xB9, 0xFA, 0xD5, 0x59, 0xAB}
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 0 }
+#define VIAL_UNLOCK_COMBO_COLS { 1, 2 }
+#define DYNAMIC_KEYMAP_LAYER_COUNT 16
+
+#define IGNORE_MOD_TAP_INTERRUPT
+#define TAPPING_FORCE_HOLD
