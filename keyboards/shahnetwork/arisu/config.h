c54da8aa3d11f6b179438c598386eccaa7fae48c
user <you@example.com>
2023-08-22 23:01:48 +1000

    ok

---
+++ keyboards/shahnetwork/arisu/config.h
@@ -0,0 +1,7 @@
+#pragma once
+
+// RGB settings
+#define RGB_MATRIX_LED_COUNT 6
+// RGB settings for the MCU
+#define WS2812_PWM_CHANNEL 1  
+
---
+++ keyboards/shahnetwork/arisu/keymaps/vial/config.h
@@ -0,0 +1,6 @@
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0x42, 0x93, 0x81, 0xC6, 0x78, 0xB6, 0x1F, 0x96}
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 0 }
+#define VIAL_UNLOCK_COMBO_COLS { 1, 2 }
+#define DYNAMIC_KEYMAP_LAYER_COUNT 8
