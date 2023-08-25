b7cd67631e5737ff672f858bbca05281fc88515f
user <you@example.com>
2023-08-25 23:33:45 +1000

    ok

--- keyboards/shahnetwork/wings/keymaps/default/keymap.c
+++ keyboards/shahnetwork/wings/keymaps/default/keymap.c
@@ -1,30 +1,15 @@
-/* Copyright 2021 alittlepeace
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 2 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
 #include QMK_KEYBOARD_H
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-  [0] = LAYOUT_all( //qwerty
+  [0] = LAYOUT_all( // qwerty
 	KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSLS,  KC_GRV,          KC_DEL,  
 	KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSPC,                   KC_PGUP,
-	OSM(MOD_LCTL), KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_ENT,                             KC_PGDN,
+	OSM(MOD_LCTL), KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_ENT,                       KC_PGDN,
 	KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,                    KC_UP,
 	KC_LGUI,          KC_LALT,          KC_SPC,  MO(3),          KC_SPC,           KC_LCTL,                           KC_LEFT, KC_DOWN, KC_RGHT 
   ),
 
-  [1] = LAYOUT_all( //dwarf
+  [1] = LAYOUT_all( // dwarf
 	KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSLS,  KC_GRV,               KC_DEL,  
 	KC_TAB,  KC_F,    KC_L,    KC_H,    KC_D,    KC_V,    KC_Z,    KC_G,    KC_O,    KC_U,    KC_DOT,  KC_LBRC, KC_RBRC, KC_BSPC,                       KC_PGUP,
 	OSM(MOD_LCTL), KC_S, KC_R, KC_N, KC_T, KC_M,    KC_P, KC_Y, KC_E, KC_I, KC_A, KC_SLASH, KC_ENT,                                                     KC_PGDN,
@@ -32,7 +17,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	KC_LGUI,          KC_LALT,          KC_SPC,  MO(3),          KC_SPC,           KC_LCTL,                           KC_LEFT, KC_DOWN, KC_RGHT 
   ),
 
-  [2] = LAYOUT_all( //dwarf hrm
+  [2] = LAYOUT_all( // dwarf hrm
 	KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSLS,  KC_GRV,                              KC_DEL,  
 	KC_TAB,  KC_F,    KC_L,    KC_H,    KC_D,    KC_V,    KC_Z,    KC_G,    KC_O,    KC_U,    KC_DOT,  KC_LBRC, KC_RBRC, KC_BSPC,           	                        KC_PGUP,
 	OSM(MOD_LCTL), LGUI_T(KC_S), LALT_T(KC_R), LSFT_T(KC_N), LCTL_T(KC_T), KC_M,    KC_P, RCTL_T(KC_Y), RSFT_T(KC_E), RALT_T(KC_I), RGUI_T(KC_A), KC_SLASH, KC_ENT,     KC_PGDN,
@@ -172,11 +157,12 @@ uint16_t get_global_quick_tap_ms(uint16_t keycode) {
     }
 };
 
-
+/*
 // To enable debug, can delete
-// void keyboard_post_init_user(void) {
-//   // Customise these values to desired behaviour
-//   debug_enable=true;
-//   debug_keyboard=true;
+ void keyboard_post_init_user(void) {
+   // Customise these values to desired behaviour
+    debug_enable=true;
+    debug_keyboard=true;
 
-// };
+ };
+ */
