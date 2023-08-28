5d57ad6e6c1b020102fedf1612c7f63fc72602a5
user <you@example.com>
2023-08-28 22:29:21 +1000

    ok

--- keyboards/shahnetwork/arisu/keymaps/default/keymap.c
+++ keyboards/shahnetwork/arisu/keymaps/default/keymap.c
@@ -5,28 +5,28 @@
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 
-	[0] = LAYOUT( // DWARF
-		KC_ESC,  KC_GRV,  KC_1, KC_2, KC_3,   KC_4,   KC_5, KC_6, KC_7, KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSLS, 
-		KC_PGUP, KC_TAB, KC_F,  KC_L,  KC_H,  KC_D,   KC_V, KC_Z, KC_G, KC_O,    KC_U,    KC_DOT,  KC_LBRC, KC_RBRC, KC_BSPC, 
-		KC_PGDN, OSM(MOD_LCTL), KC_S, KC_R, KC_N, KC_T, KC_M,    KC_P, KC_Y, KC_E, KC_I, KC_A, KC_SLASH, KC_ENT, 
-		         KC_LSFT, KC_X, KC_J, KC_B, KC_K, KC_Q, KC_Q, KC_C, KC_W, KC_QUOTE, KC_COMMA, KC_SEMICOLON, KC_UP,   KC_RSFT, 
-		         KC_LCTL, KC_LGUI,    KC_LALT,  MO(5),  KC_SPC,    MO(4),                        KC_LEFT, KC_DOWN, KC_RGHT
-	),
-
-	[1] = LAYOUT( // QWERTY default
-		KC_ESC,  KC_GRV,  KC_1, KC_2, KC_3,   KC_4,   KC_5, KC_6, KC_7, KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC, 
-		KC_PGUP, KC_TAB,  KC_Q, KC_W, KC_E,   KC_R,   KC_T, KC_Y, KC_U, KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS, 
-		KC_PGDN, KC_CAPS, KC_A, KC_S, KC_D,   KC_F,   KC_G, KC_H, KC_J, KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_ENT, 
-		         KC_LSFT, KC_Z, KC_X, KC_C,   KC_V,   KC_B, KC_B, KC_N, KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_UP,   KC_RSFT, 
-		         KC_LCTL, KC_LGUI,    KC_LALT,  KC_SPC,  KC_SPC,    MO(4),                        KC_LEFT, KC_DOWN, KC_RGHT
-	),
-
-	[2] = LAYOUT( // DWARF HRM
-	    KC_ESC,  KC_GRV,  KC_1, KC_2, KC_3,   KC_4,   KC_5, KC_6, KC_7, KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSLS, 
+	[0] = LAYOUT( // DWARF HRM
+	    KC_DEL,  KC_ESC,  KC_1, KC_2, KC_3,   KC_4,   KC_5, KC_6, KC_7, KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSLS, 
 		KC_PGUP, KC_TAB, KC_F,  KC_L,  KC_H,  KC_D,   KC_V, KC_Z, KC_G, KC_O,    KC_U,    KC_DOT,  KC_LBRC, KC_RBRC, KC_BSPC, 
 		KC_PGDN, OSM(MOD_LCTL), LGUI_T(KC_S), LALT_T(KC_R), LSFT_T(KC_N), LCTL_T(KC_T), KC_M,    KC_P, RCTL_T(KC_Y), RSFT_T(KC_E), RALT_T(KC_I), RGUI_T(KC_A), KC_SLASH, KC_ENT, 
 		         KC_LSFT, KC_X, KC_J, KC_B, KC_K, KC_Q, KC_Q, KC_C, KC_W, KC_QUOTE, KC_COMMA, KC_SEMICOLON, KC_UP,   KC_RSFT, 
-		         KC_LCTL, KC_LGUI,    KC_LALT,  MO(5),  KC_SPC,    MO(4),                        KC_LEFT, KC_DOWN, KC_RGHT
+		         KC_LCTL, KC_LGUI,    KC_LALT,  MO(5),  KC_SPC,    MO(4),                  KC_LEFT, KC_DOWN, KC_RGHT
+	),
+
+	[1] = LAYOUT( // DWARF
+		KC_DEL,  KC_ESC,  KC_1, KC_2, KC_3,   KC_4,   KC_5, KC_6, KC_7, KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSLS, 
+		KC_PGUP, KC_TAB, KC_F,  KC_L,  KC_H,  KC_D,   KC_V, KC_Z, KC_G, KC_O,    KC_U,    KC_DOT,  KC_LBRC, KC_RBRC, KC_BSPC, 
+		KC_PGDN, OSM(MOD_LCTL), KC_S, KC_R, KC_N, KC_T, KC_M,    KC_P, KC_Y, KC_E, KC_I, KC_A, KC_SLASH, KC_ENT, 
+		         KC_LSFT, KC_X, KC_J, KC_B, KC_K, KC_Q, KC_Q, KC_C, KC_W, KC_QUOTE, KC_COMMA, KC_SEMICOLON, KC_UP,   KC_RSFT, 
+		         KC_LCTL, KC_LGUI,    KC_LALT,  MO(5),  KC_SPC,    MO(4),                   KC_LEFT, KC_DOWN, KC_RGHT
+	),
+
+	[2] = LAYOUT( // QWERTY 
+		KC_DEL,  KC_ESC,  KC_1, KC_2, KC_3,   KC_4,   KC_5, KC_6, KC_7, KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC, 
+		KC_PGUP, KC_TAB,  KC_Q, KC_W, KC_E,   KC_R,   KC_T, KC_Y, KC_U, KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS, 
+		KC_PGDN, OSM(MOD_LCTL), KC_A, KC_S, KC_D,   KC_F,   KC_G, KC_H, KC_J, KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_ENT, 
+		         KC_LSFT, KC_Z, KC_X, KC_C,   KC_V,   KC_B, KC_B, KC_N, KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_UP,   KC_RSFT, 
+		         KC_LCTL, KC_LGUI,    KC_LALT,  MO(5),  KC_SPC,    MO(4),                  KC_LEFT, KC_DOWN, KC_RGHT
 	),
 
 	[3] = LAYOUT(
@@ -38,19 +38,19 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	),
 
 	[4] = 		LAYOUT( // Right MOD
-		_______, _______, KC_F1,   KC_F2,   KC_F3,    KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,    KC_F10,  KC_F11,  KC_F12,  QK_BOOT, 
-		_______, KC_CAPS, DF(0), DF(1), DF(2),  _______, _______, _______, _______, _______, _______,  KC_PSCR, KC_SCRL, KC_PAUS, _______, 
-		_______, _______, KC_MUTE, KC_VOLD, KC_VOLU, KC_MPLY, _______, _______, _______, _______, _______,  KC_HOME, KC_END,  _______, 
+		DF(0), _______, KC_F1,   KC_F2,   KC_F3,    KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,    KC_F10,  KC_F11,  KC_F12,  KC_GRV, 
+		DF(1), KC_CAPS, _______, _______, _______, _______, _______, _______, _______, _______, _______,  KC_PSCR, KC_SCRL, KC_PAUS, QK_BOOT, 
+		DF(2), _______, KC_MUTE, KC_VOLD, KC_VOLU, KC_MPLY, _______, _______, _______, _______, _______,  KC_HOME, KC_END,  _______, 
 		         _______, C(KC_Z), C(KC_X), C(KC_C), C(KC_V), _______, _______, _______, _______, _______,  KC_INS,  KC_DEL,  _______, _______, 
 		         _______, _______,          _______, _______,  _______, _______,                                      _______, _______, _______
 	),
 	
 	[5] = LAYOUT( // L spacebar mod
 		_______, RGB_TOG, RGB_MOD, RGB_HUI, RGB_SAI, RGB_VAI, RGB_SPI, _______, _______, _______, _______, _______, _______, _______, _______, 
-		_______, _______, _______, _______, _______, _______, _______, _______, KC_7, KC_8, KC_9, _______, _______, _______, KC_DEL, 
-		_______, _______, KC_MUTE, KC_VOLD, KC_VOLU, KC_MPLY, _______, _______, KC_4, KC_5, KC_6, _______, _______, _______, 
-		         _______, C(KC_Z), C(KC_X), C(KC_C), C(KC_V), _______, KC_0, KC_1, KC_2, KC_3, _______, _______, KC_PGUP, _______, 
-		         _______, _______,          _______, _______, _______, _______,                                     KC_HOME, KC_PGDN, KC_END
+		_______, KC_ESC, _______, _______, _______, _______, _______, _______, KC_7, KC_8, KC_9, _______, _______, _______, KC_DEL, 
+		_______, KC_CAPS, KC_MUTE, KC_VOLD, KC_VOLU, KC_MPLY, _______, _______, KC_4, KC_5, KC_6, _______, _______, _______, 
+		         _______, C(KC_Z), C(KC_X), C(KC_C), C(KC_V), _______, _______, KC_0, KC_1, KC_2, KC_3, _______, KC_PGUP, _______, 
+		         _______, _______,          _______, _______, _______, _______,                         KC_HOME, KC_PGDN, KC_END
 	),
 
 	[6] = LAYOUT(
@@ -125,17 +125,21 @@ bool achordion_chord(uint16_t tap_hold_keycode,
     case LALT_T(KC_R):
     case LSFT_T(KC_N):
     case LCTL_T(KC_T):
-      if (other_keycode == KC_6 || other_keycode == KC_6) { return false; }
+      if (other_record->event.key.row == 0 && other_record->event.key.col == 7) { return false; } // number 6
+      if (other_record->event.key.row == 3 && other_record->event.key.col == 6) { return true; } // right q (DWARF) or b (QWERTY)
       break;
       
     case RCTL_T(KC_Y):  
     case RSFT_T(KC_E):
     case RALT_T(KC_I):
     case RGUI_T(KC_A):
-      if (other_keycode == KC_6 || other_keycode == KC_6) { return true; }
+      if (other_record->event.key.row == 0 && other_record->event.key.col == 7) { return true; } // number 6
+      if (other_record->event.key.row == 3 && other_record->event.key.col == 6) { return false; } // right q (DWARF) or b (QWERTY)
       break;
   }
-   if (tap_hold_record->event.key.row % (MATRIX_ROWS / 2) >= 4) { return true; } // Ignore bottom row and thumbcluster
+  
+  if (other_record->event.key.row == 4) { return true; } // Allow the bottom row to be used as the second key for same side
+  if (tap_hold_record->event.key.row == 4) { return true; } // Allow the bottom row to be used as the first key for same side
   return achordion_opposite_hands(tap_hold_record, other_record);
 };
 
@@ -191,4 +195,14 @@ uint16_t get_global_quick_tap_ms(uint16_t keycode) {
             return 0;  // global_quick_tap is not applied
     }
 };
+
+
+// To enable debug, can delete
+ void keyboard_post_init_user(void) {
+   // Customise these values to desired behaviour
+    debug_enable=true;
+    debug_keyboard=false;
+    debug_matrix=false;
+ };
+
 
