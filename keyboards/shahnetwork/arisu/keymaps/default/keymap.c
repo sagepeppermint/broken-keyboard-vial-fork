b72c55ab04bf8d9e7dfd59a584891d7281aa3bc4
user <you@example.com>
2023-08-30 19:38:53 +1000

    ok

--- keyboards/shahnetwork/arisu/keymaps/default/keymap.c
+++ keyboards/shahnetwork/arisu/keymaps/default/keymap.c
@@ -10,7 +10,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 		KC_PGUP, KC_TAB, KC_F,  KC_L,  KC_H,  KC_D,   KC_V, KC_Z, KC_G, KC_O,    KC_U,    KC_DOT,  KC_LBRC, KC_RBRC, KC_BSPC, 
 		KC_PGDN, OSM(MOD_LCTL), LGUI_T(KC_S), LALT_T(KC_R), LSFT_T(KC_N), LCTL_T(KC_T), KC_M,    KC_P, RCTL_T(KC_Y), RSFT_T(KC_E), RALT_T(KC_I), RGUI_T(KC_A), KC_SLASH, KC_ENT, 
 		         KC_LSFT, KC_X, KC_J, KC_B, KC_K, KC_Q, KC_Q, KC_C, KC_W, KC_QUOTE, KC_COMMA, KC_SEMICOLON, KC_UP,   KC_RSFT, 
-		         KC_LCTL, KC_LGUI,    KC_LALT,  MO(5),  KC_SPC,    MO(4),                  KC_LEFT, KC_DOWN, KC_RGHT
+		         KC_LALT, KC_LGUI,    MO(5), KC_SPC,  KC_SPC,    MO(4),                  KC_LEFT, KC_DOWN, KC_RGHT
 	),
 
 	[1] = LAYOUT( // DWARF
@@ -18,7 +18,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 		KC_PGUP, KC_TAB, KC_F,  KC_L,  KC_H,  KC_D,   KC_V, KC_Z, KC_G, KC_O,    KC_U,    KC_DOT,  KC_LBRC, KC_RBRC, KC_BSPC, 
 		KC_PGDN, OSM(MOD_LCTL), KC_S, KC_R, KC_N, KC_T, KC_M,    KC_P, KC_Y, KC_E, KC_I, KC_A, KC_SLASH, KC_ENT, 
 		         KC_LSFT, KC_X, KC_J, KC_B, KC_K, KC_Q, KC_Q, KC_C, KC_W, KC_QUOTE, KC_COMMA, KC_SEMICOLON, KC_UP,   KC_RSFT, 
-		         KC_LCTL, KC_LGUI,    KC_LALT,  MO(5),  KC_SPC,    MO(4),                   KC_LEFT, KC_DOWN, KC_RGHT
+		         KC_LALT, KC_LGUI,    MO(5), KC_SPC,  KC_SPC,    MO(4),                  KC_LEFT, KC_DOWN, KC_RGHT
 	),
 
 	[2] = LAYOUT( // QWERTY 
@@ -26,7 +26,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 		KC_PGUP, KC_TAB,  KC_Q, KC_W, KC_E,   KC_R,   KC_T, KC_Y, KC_U, KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS, 
 		KC_PGDN, OSM(MOD_LCTL), KC_A, KC_S, KC_D,   KC_F,   KC_G, KC_H, KC_J, KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_ENT, 
 		         KC_LSFT, KC_Z, KC_X, KC_C,   KC_V,   KC_B, KC_B, KC_N, KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_UP,   KC_RSFT, 
-		         KC_LCTL, KC_LGUI,    KC_LALT,  MO(5),  KC_SPC,    MO(4),                  KC_LEFT, KC_DOWN, KC_RGHT
+		         KC_LALT, KC_LGUI,    MO(5), KC_SPC,  KC_SPC,    MO(4),                  KC_LEFT, KC_DOWN, KC_RGHT
 	),
 
 	[3] = LAYOUT(
@@ -47,7 +47,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	
 	[5] = LAYOUT( // L spacebar mod
 		_______, RGB_TOG, RGB_MOD, RGB_HUI, RGB_SAI, RGB_VAI, RGB_SPI, _______, _______, _______, _______, _______, _______, _______, _______, 
-		_______, KC_ESC, _______, _______, _______, _______, _______, _______, KC_7, KC_8, KC_9, _______, _______, _______, KC_DEL, 
+		_______, _______, _______, _______, _______, _______, _______, _______, KC_7, KC_8, KC_9, _______, _______, _______, _______, 
 		_______, KC_CAPS, KC_MUTE, KC_VOLD, KC_VOLU, KC_MPLY, _______, _______, KC_4, KC_5, KC_6, _______, _______, _______, 
 		         _______, C(KC_Z), C(KC_X), C(KC_C), C(KC_V), _______, _______, KC_0, KC_1, KC_2, KC_3, _______, KC_PGUP, _______, 
 		         _______, _______,          _______, _______, _______, _______,                         KC_HOME, KC_PGDN, KC_END
