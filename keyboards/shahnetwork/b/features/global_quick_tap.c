548ff78761d1a7da0ce735a10b94c1b0bfb6571c
sagepeppermint <101568628+sagepeppermint@users.noreply.github.com>
2023-12-08 18:58:24 +1100

    Update global_quick_tap.c

--- keyboards/shahnetwork/b/features/global_quick_tap.c
+++ keyboards/shahnetwork/b/features/global_quick_tap.c
@@ -2,15 +2,15 @@
 
 
 static struct {
-    uint16_t keycode;
     bool disabled;
     bool key_registered;
-} gqt_state = {0, false, false};
+    bool double_pressed
+} gqt_state = {false, false, false};
 
 void reset_global_quick_tap_state(void) {
-    gqt_state.keycode = 0;
     gqt_state.disabled = false;
     gqt_state.key_registered = false;
+    gqt_state.double_pressed = false;
 }
 
 static uint16_t prev_press_time = 0;
@@ -20,43 +20,50 @@ bool process_global_quick_tap(uint16_t keycode, keyrecord_t* record) {
     if (record->event.key.row % (MATRIX_ROWS / 2) >= 4) { // disable bottom rows in a split keyboard
         dprintln("Global-quick-tap: skip bottom rows"); 
         return true; 
-        }
-        
+    }
+
     uint16_t quick_tap_ms = get_global_quick_tap_ms(keycode);
-    
+
     if (record->event.pressed) { // on keydown
 
         /* Any key down. Recording key press times */
         uint16_t time_diff = record->event.time - prev_press_time;
         prev_press_time = record->event.time;
 
-        if (!gqt_state.disabled) {
-
-            if (quick_tap_ms > 0) {
-                dprintf("Global-quick-tap: Last key press was %dms ago, global_quick_tap is %dms. ", time_diff, quick_tap_ms);
-                if (quick_tap_ms > time_diff) {
-                    dprintf("Disabling hold-tap for 0x%04X\n", keycode);
-                    register_code16(QK_MOD_TAP_GET_TAP_KEYCODE(keycode));
-                    gqt_state.key_registered = true;
-                    return false;
-                } else {
-                    dprintf("Hold-taps and mod combos are allowed for key 0x%04X\n", keycode);
-                    gqt_state.disabled = true;
-                }
-                gqt_state.keycode = keycode;
+        if (quick_tap_ms > 0) {
+            dprintf("Global-quick-tap: Last key press was %dms ago, global_quick_tap is %dms.", time_diff, quick_tap_ms);
+            if ((quick_tap_ms > time_diff) && !gqt_state.disabled) {
+                dprintf("Disabling hold-tap for 0x%04X\n", keycode);
+                register_code16(QK_MOD_TAP_GET_TAP_KEYCODE(keycode));
+                gqt_state.key_registered = true;
+                return false;
+            } else {
+                dprintf("Hold-taps and mod combos are allowed for key 0x%04X\n", keycode);
+                if (!gqt_state.disabled){gqt_state.disabled = true};
+                if (gqt_state.disabled){gqt_state.double_pressed = true};
             }
         }
-    } 
+    }
     else { // on keyup
         if (quick_tap_ms > 0) {
-            dprintf("Unregister  key 0x%04X\n", keycode);
-            unregister_code16(QK_MOD_TAP_GET_TAP_KEYCODE(keycode));
-            reset_global_quick_tap_state();
+            if (gqt_state.key_registered){
+                dprintf("Unregister key 0x%04X\n", keycode);
+                unregister_code16(QK_MOD_TAP_GET_TAP_KEYCODE(keycode));
+                gqt_state.key_registered = false;
+            }
+            if (gqt_state.disabled && !gqt_state.double_pressed){
+                gqt_state.disabled = false;
+            }
+            if (gqt_state.double_pressed){
+                gqt_state.double_pressed = false;
+            }
+            
+//            reset_global_quick_tap_state();
         }
     }
 
     return true;
-};
+}
 
 __attribute__((weak))
 uint16_t get_global_quick_tap_ms(uint16_t keycode) {
